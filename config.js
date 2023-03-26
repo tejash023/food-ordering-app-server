695c2a3cee4279c62900d18a43ea4037b3c64c23
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-26 13:28:24 +0530

    added routes and controllers

---
+++ config.js
@@ -0,0 +1,3 @@
+export const restaurantURL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
+
+export const menuURL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&submitAction=ENTER&restaurantId=${restaurantId}`;
--- controller/api-controller.js
+++ controller/api-controller.js
@@ -1,7 +1,60 @@
+import fetch from "cross-fetch";
 export const getRestaurant = async (req, res) => {
-  return res.status(200).json({
-    message: "success",
-  });
+  try {
+    const { lat, lng } = req.query;
+    const URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=DESKTOP_WEB_LISTING`;
+
+    const response = await fetch(URL, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+      },
+    });
+    const data = await response.json();
+
+    return res.status(200).json({
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(501).json({
+      success: false,
+      message: "Something went wrong",
+      data: {},
+      error: error,
+    });
+  }
 };
 
-export const getMenu = async (req, res) => {};
+export const getMenu = async (req, res) => {
+  try {
+    const { lat, lng, restaurantId } = req.query;
+
+    const URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&submitAction=ENTER&restaurantId=${restaurantId}`;
+
+    const response = await fetch(URL, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+      },
+    });
+
+    const data = await response.json();
+
+    return res.status(200).json({
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(501).json({
+      success: false,
+      message: "Something went wrong",
+      data: {},
+      error: error,
+    });
+  }
+};
