f6e4dd1a30d98720147759ace2b08e34e4e2321a
Tejashwa Rajvardhan <36923392+tejash023@users.noreply.github.com>
2023-08-22 01:07:40 +0530

    added category api endpoint

--- index.js
+++ index.js
@@ -64,6 +64,35 @@ app.get("/api/menu", (req, res) => {
     });
 });
 
+app.get("/api/category", (req, res) => {
+  const { lat, lng, collectionId } = req.query;
+
+  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&collection=${collectionId}&isNewCollectionFlow=true&sortBy=&filters=&type=rcv2&offset=0&page_type=null`;
+
+  fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
+
+
 app.get("/", (req, res) => {
   res.json({ test: "Welcome to Go Foods!" });
 });
